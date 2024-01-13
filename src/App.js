df067fd531c18a26a897d065d3e8ac9bc5eaba49
g6y116 <g6y116@naver.com>
2024-01-14 01:35:17 +0900

    setting : 개발 환경 세팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 function App() {
+  const [currentTime, setCurrentTime] = useState('');
+
+  useEffect(() => {
+    console.log(process.env.REACT_APP_API_MODE);
+    // /api/time 엔드포인트로 GET 요청을 보냄
+    axios.get('/api/time')
+      .then(response => {
+        setCurrentTime(response.data);
+      })
+      .catch(error => {
+        console.error('API 호출 중 오류 발생:', error);
+      });
+  }, []); // 빈 배열을 전달하여 컴포넌트가 처음 마운트될 때만 실행
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>현재 시간</h1>
+      <p>{currentTime}</p>
     </div>
   );
 }
