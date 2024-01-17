6af94686b2ac9234799bb80f80127ec7e4a8624d
eunv <dmsql1091@naver.com>
2024-01-17 14:56:55 +0900

    feat : 헤더 조건 추가

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,52 +1,76 @@
-import React, { useState } from 'react'
-import styles from './Header.module.scss'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import styles from './Header.module.scss';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { headerState } from '../../states/header/headerState';
+import "react-datepicker/dist/react-datepicker.module.css"
 
 const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [headerSettings, setHeaderSettings] = useRecoilState(headerState);
+  const { showDefalut, showFeatures,showCreate } = headerSettings;
   const [isHovered, setIsHovered] = useState(false);
+
+  // 페이지 이동을 위한 함수
+  const navigateTo = (path) => () => {
+    if(path === '/main') {
+      setIsHovered(false);
+    }
+    navigate(path);
+  };
+
+    // 현재 경로에 따라 헤더 상태 업데이트
+    useEffect(() => {
+      if (location.pathname.includes('/register')) {
+        setHeaderSettings({ showDefalut: false, showFeatures: true });
+      } else {
+        setHeaderSettings({ showDefalut: true, showFeatures: false });
+      }
+    }, [location, setHeaderSettings]);
+
   return (
     <section className={styles.header}>
       <div className={styles.header__container}>
-        <Link to="main" className={styles.header_logo}>
-          {/* <img style={{width:"160px"}} src='banner/DAMON.png'/> */}
+        <div className={styles.header_logo} onClick={navigateTo('/main')}>
           DAMON
-        </Link>
-        <div className={styles.header__content}>
-          <nav className={styles.header__nav}>
-            <ul className={styles.gnb}
-              onMouseEnter={() => setIsHovered(true)}
-              onMouseLeave={() => setIsHovered(false)}>
-              <li className={styles.header__menu}><Link href=''>전체 리뷰</Link>
-                <ul className={isHovered ? styles.subVisible : styles.sub}>
-                </ul>
-              </li>
-              <li className={styles.header__menu}><Link href=''>커뮤니티</Link>
-                <ul className={isHovered ? styles.subVisible : styles.sub}>
-                </ul>
-              </li>
-              <li className={styles.header__menu2}>등록
-                <ul className={isHovered ? styles.subVisible : styles.sub}>
-                  <li><Link>리뷰 등록</Link></li>
-                  <li><Link to="register/schedule">일정 등록</Link></li>
-                  <li><Link>게시글 등록</Link></li>
-                </ul>
-              </li>
-              <li className={styles.header__menu2}>마이룸
-                <ul className={isHovered ? styles.subVisible : styles.sub}>
-                  <li><Link>내 리뷰</Link></li>
-                  <li><Link>내 일정</Link></li>
-                  <li><Link>커뮤니티</Link></li>
-                </ul>
-              </li>
-            </ul>
-          </nav>
-          <div>
-            <a className={styles.header__logout}>로그아웃</a>
-          </div>
         </div>
+        {showDefalut &&
+          <div className={styles.header__content}>
+            <nav className={styles.header__nav}>
+              <ul className={styles.gnb}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}>
+                <li className={styles.header__menu} onClick={navigateTo('/review')}>전체 리뷰</li>
+                <li className={styles.header__menu} onClick={navigateTo('/community')}>커뮤니티</li>
+                <li className={styles.header__menu2}>등록
+                  <ul className={isHovered ? styles.subVisible : styles.sub}>
+                    <li onClick={navigateTo('/register/review')}>리뷰 등록</li>
+                    <li onClick={navigateTo('/register/calender')}>일정 등록</li>
+                    <li onClick={navigateTo('/register/post')}>게시글 등록</li>
+                  </ul>
+                </li>
+                <li className={styles.header__menu2}>마이룸
+                  <ul className={isHovered ? styles.subVisible : styles.sub}>
+                    <li onClick={navigateTo('/my/reviews')}>내 리뷰</li>
+                    <li onClick={navigateTo('/my/calender')}>내 일정</li>
+                    <li onClick={navigateTo('/my/community')}>내 커뮤니티</li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+            <div onClick={navigateTo('/logout')} className={styles.header__logout}>로그아웃</div>
+          </div>
+        }
+        {showFeatures &&
+          <div className={styles.header__btns}>
+            <button className={styles.cancel_btn}>취소</button>
+            {showCreate && <button className={styles.confirm_btn}>등록</button>}
+          </div>
+        }
+
       </div>
-      <div className={isHovered ? styles.nav__backgroundVisible : styles.nav__background}>
-      </div>
+      {showDefalut && <div className={isHovered ? styles.nav__backgroundVisible : styles.nav__background}></div>}
     </section>
   )
 }
