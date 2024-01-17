deb6f488645637f0f6de2765a85a20983cf2539c
eunv <dmsql1091@naver.com>
2024-01-17 15:07:27 +0900

    feat : 등록세팅 사이드바

---
+++ src/components/calender/create-days/CreateDays.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './CreateDays.module.scss'
+
+const CreateDays = () => {
+  return (
+    <div>CreateDays</div>
+  )
+}
+
+export default CreateDays
---
+++ src/components/sidebars/sidebar2/CreateSidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './CreateSidebar.module.scss'
+
+const CreateSidebar = () => {
+  return (
+    <div className={styles.container}>
+      <section>
+        <div>
+          ?
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default CreateSidebar
