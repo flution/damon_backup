ea0d3e90f3f5bb5f5e2c889191b29488f6e2786a
eunv <dmsql1091@naver.com>
2024-01-17 15:12:20 +0900

    fix : 라우터 변경

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Main from './pages/main/Main';
-import RegisterCalender from './pages/schedules/register-schedule/RegisterCalender';
+import RegisterCalender from './pages/calenders/register-calender/RegisterCalender';
 
 
 function App() {
