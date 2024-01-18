50c4940e20e6a063a83ffadbf572121893b1441e
eunv <dmsql1091@naver.com>
2024-01-18 22:18:01 +0900

    feat : 일정 등록 페이지 구조 변경

--- src/components/calendar/create-days/CreateDays.jsx
+++ src/components/calendar/create-days/CreateDays.jsx
@@ -1,28 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import styles from './CreateDays.module.scss'
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { calendarInfoState } from '../../../states/calendar/calendarInfoState';
+import { calendarInfoState, clickedDateState } from '../../../states/calendar/calendarInfoState';
 
 const CreateDays = ({ index }) => {
 
   const [calenderInfo, setCalenderInfo] = useRecoilState(calendarInfoState);
+  const [clickedDate, setClickedDate] = useRecoilState(clickedDateState);
+
   const [date, setDate] = useState("");
+
+  const handleClickedDate = () => {
+    setClickedDate(index + 1);
+  }
 
   const getDate = () => {
     const date = new Date(calenderInfo.startDate);
     const calcDate = new Date(date.setDate(date.getDate() + index));
-    // const date = new Date((calenderInfo.startDate).getDate() + index + 1);
 
     setDate(calcDate.toLocaleDateString());
   }
 
   useEffect(() => {
     getDate();
-  }, [])
+  }, [calenderInfo.endDate])
 
 
   return (
-    <div className={styles.container}>
+    <div key={index + 1}
+      onClick={handleClickedDate}
+      className={`${styles.container} ${(index+1) == clickedDate ? styles.btnActive : ""}`}
+    >
       <p>Day{index + 1}</p>
       {date}
 
