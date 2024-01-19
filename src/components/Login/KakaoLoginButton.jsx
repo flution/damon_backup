4bab66581fdeceff112eb43fefe03b859adc5288
flution <flution221@gmail.com>
2024-01-19 14:34:45 +0900

    fix : 카카오 로그인 구현 수정

---
+++ src/components/Login/KakaoLoginButton.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './LoginButton.scss'; // 위에서 작성한 CSS 파일 import
+import kakaoLogo from '../../assets/kakao_Logo_black.png';
+
+function KakaoLoginButton() {
+  // 로그인 로직을 여기에 추가할 수 있습니다.
+  const handleLogin = () => {
+    console.log('카카오 로그인 로직을 구현하세요.');
+  };
+
+  return (
+    <button className="social-login-button kakao-login-button">
+      <img src={kakaoLogo} alt="Logo" className="button-logo" />
+      카카오로 시작하기
+    </button>
+  );
+}
+
+export default KakaoLoginButton;
