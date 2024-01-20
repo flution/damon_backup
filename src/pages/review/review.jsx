b9b65730a4599e389792df4b1326988bce3c2c43
Gwan soon Park <gwansoonpark@Gwanui-MacBookPro.local>
2024-01-20 16:30:00 +0900

    feat : 소셜 로그인 연동

---
+++ src/pages/review/review.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './review.module.scss';
+
+function Review() {
+  return (
+    <div className={styles.reviewContainer}>
+      {/* 리뷰 페이지 관련 내용 */}
+    </div>
+  );
+}
+
+export default Review;
