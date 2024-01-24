ef59451de13ff9e81c2c48d7e9813519d2912124
flution <flution221@gmail.com>
2024-01-24 15:56:39 +0900

    fix : 로그인 페이지 구현 수정중

--- src/components/Login/KakaoLoginButton.jsx
+++ src/components/Login/KakaoLoginButton.jsx
@@ -4,12 +4,12 @@ import './LoginButton.scss';
 import kakaoLogo from '../../assets/Kakao_Logo.png';
 
 function KakaoLoginButton() {
-  const navigate = useNavigate(); // 네비게이션 함수 사용
+  ; // 네비게이션 함수 사용
 
   // 버튼 클릭 시 카카오 로그인 페이지로 리다이렉트
   const handleLogin = () => {
     const REST_API_KEY = 'ab3a8af0dfdacf22fc47199199e5bb2f';
-    const REDIRECT_URI = 'http://localhost:8080/login/oauth2/code/kakao'; // 리다이렉트 URI 설정
+    const REDIRECT_URI = 'http://localhost:8080/login/oauth2/code/kakao'; // 클라이언트에서 사용하는 리다이렉트 URI
     const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
     window.location.href = link;
   };
