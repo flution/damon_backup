50a0cdc89e7081a614f2e6e16d578c7b4ac2a727
flution <flution221@gmail.com>
2024-01-24 16:20:05 +0900

    Revert "fix : 로그인 페이지 구현 수정중"
    This reverts commit ef59451de13ff9e81c2c48d7e9813519d2912124.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Layout from './components/layout/Layout';
+import axios from 'axios';
+import { Routes, Route } from 'react-router-dom';
 import Login from './pages/login/Login'
+import Layout from './components/layout/Layout';
 import Main from './pages/main/Main';
-import Review from './pages/review/Review';
+import Review from './pages/review/review';
 import RegisterCalendar from './pages/calendars/register-calendar/RegisterCalendar';
-import KakaoRedirectHandler from './components/login/KakaoLoginRedirect'
 
 
 
@@ -13,14 +13,14 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<Login />} />
-      <Route path='/login/oauth2/code/kakao' element={<KakaoRedirectHandler />} />
 
       <Route path='/' element={<Layout />}>
-        <Route path='main' element={<Main />} />
+        <Route index element={<Main />} />
         <Route path='review/:id' element={<Review />} />
-        <Route path='register/calendar' element={<RegisterCalendar />} />
+        <Route path='register/calender' element={<RegisterCalendar />} />
           
         {/* <Route path='view/myschedule' element={<MySchedule />} /> */}
+
       </Route>
     </Routes>
   );
--- src/components/Login/NaverLoginButton.jsx
+++ src/components/Login/NaverLoginButton.jsx
@@ -10,7 +10,8 @@ function NaverLoginButton() {
   // 네이버 로그인을 위한 URL 구성
   const CLIENT_ID = 'spdOdDxRE5b1jvsH5qAi'; // 네이버 클라이언트 ID 입력
   const REDIRECT_URI = 'http://localhost:8080/login/oauth2/code/naver'; // 실제 배포 시 변경 필요
-  const naverAuthUrl = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
+  const STATE = 'RANDOM_STATE'; // 보안을 위한 상태 토큰, 실제로는 랜덤하게 생성
+  const naverAuthUrl = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&state=${STATE}`;
 
   // 로그인 버튼 클릭 핸들러
   const handleLogin = () => {
